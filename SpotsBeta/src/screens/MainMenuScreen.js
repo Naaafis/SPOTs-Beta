61dd174f554ebd926d66d98eb74262520afd6057
Nafis Abeer <cheffbcookin@dhcp-wifi-8021x-155-41-112-73.bu.edu>
2022-04-26 11:53:43 -0400

    front end

---
+++ SpotsBeta/src/screens/MainMenuScreen.js
@@ -0,0 +1,32 @@
+import React , {Component} from 'react';
+import {View, Text, Button, StyleSheet} from 'react-native';
+//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
+class MainMenuScreen extends Component{
+  render(){
+    return (
+      <View style={{flex: 1, alignItems: 'center'}}>
+        <Text style={style.title}>Main Menu</Text>
+        <Button title="Edit Profile" onPress={() => this.props.navigation.push('Edit Profile')} />
+        <Button title="View Profile" onPress={() => this.props.navigation.push('View Profile')} />
+        <Button title="Friends" onPress={() => this.props.navigation.push('Friends')} />
+        <Button
+          title="Add Friends"
+          onPress={() => this.props.navigation.push('Add Friends')}
+        />
+        <Button title="Map" onPress={() => this.props.navigation.push('Map')} />
+        <Button title="Chat" onPress={() => this.props.navigation.push('Chat')} />
+        <Button title="Logout" onPress={() => this.props.navigation.navigate('Home')} />
+      </View>
+    );
+  }
+}
+
+export {MainMenuScreen};
+
+const style = StyleSheet.create({
+  title: {
+    marginTop: 20,
+    marginBottom: 20,
+    fontSize: 48,
+  },
+});
