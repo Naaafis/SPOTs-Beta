b386392d555b4ade242940e6c65c2e95f2828bd8
Nafis Abeer <cheffbcookin@dhcp-wifi-8021x-155-41-112-73.bu.edu>
2022-04-27 10:08:15 -0400

    Updated and can change contents of DB

--- UsersDB/Users_DB_test.py
+++ UsersDB/Users_DB_test.py
@@ -10,14 +10,14 @@ timestamp = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
 mydb = mysql.connector.connect(
   host="localhost",
   user="root",
-  password="#########", #for the purposes of github, I will not include this in the commit 
+  password="PASSWORD", #for the purposes of github, I will not include this in the commit 
   database="SpotsUsers"
 )
 
 mycursor = mydb.cursor()
 
-sql = "INSERT INTO users (id, fullname, dob, pronouns, email, password, datejoined) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-val = (1, "John", "January 2nd, 1998", "he/him", "john@doe.com", "1234", timestamp)
+sql = "INSERT INTO users (id, fullname, dob, pronouns, email, abt, datejoined) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+val = (1, "John", "January 2nd, 1998", "he/him", "john@doe.com", "likes pies", timestamp)
 mycursor.execute(sql, val)
 
 mydb.commit()
--- UsersDB/Users_table.sql
+++ UsersDB/Users_table.sql
@@ -1,21 +1,21 @@
-CREATE DATABASE IF NOT EXISTS SpotsUsers;
-USE SpotsUsers;
+CREATE DATABASE IF NOT EXISTS spotsusers;
+USE spotsusers;
 
 CREATE TABLE IF NOT EXISTS users (
-  id int,
+  id int UNIQUE,
   fullname varchar(255),
   dob varchar(255),
   pronouns varchar(255),
   email varchar(255),
   abt varchar(255),
   datejoined TIMESTAMP,
-  CONSTRAINT pk_id PRIMARY KEY (id)
+  CONSTRAINT id PRIMARY KEY (id)
 );
 
 
 CREATE TABLE IF NOT EXISTS user_friend (
-  id_user int,
-  id_friend int,
-  CONSTRAINT fk_id FOREIGN KEY (id_user) REFERENCES users(id),
-  CONSTRAINT fk_id FOREIGN KEY (id_friend) REFERENCES users(id)
+  id_user int UNIQUE,
+  id_friend int UNIQUE,
+  CONSTRAINT id_user FOREIGN KEY (id_user) REFERENCES users(id),
+  CONSTRAINT id_friend FOREIGN KEY (id_friend) REFERENCES users(id)
 );
